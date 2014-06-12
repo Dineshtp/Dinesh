88ba12f39363509d4ba9285cdd9672c5533a7a06
Dinesh Kumar <tpdinesh@outlook.com>
2014-06-12 14:04:24 +0530

    initial

---
+++ sample_app/spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe PagesController, :type => :controller do
+  render_views
+  describe "GET 'home'" do
+    it "returns http success" do
+      get 'home'
+      expect(response).to be_success
+    end
+    it "should have the right title" do
+      response.should have_selector("title", :content => "Ruby on rails Tutorial Sample App | Home")
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "returns http success" do
+      get 'contact'
+      expect(response).to be_success
+    it "should have the right title" do
+      response.should have_selector("title", :content => "Ruby on rails Tutorial Sample App | Contact")
+    end
+   end
+
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      expect(response).to be_success
+    end
+    it "should have the right title" do
+      response.should have_selector("title", :content => "Ruby on rails Tutorial Sample App | About")
+    end
+  end
+
+end
